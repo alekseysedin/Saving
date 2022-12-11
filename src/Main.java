a17f33656fc3761e9987867ef2db9ac0a39c5899
Aleksey Sedin <alekseysedin@outlook.com>
2022-12-11 15:45:49 +0400

    add2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,42 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
 
-    public static void saveGame(String way, GameProgress game) {
+    public static void saveGame(GameProgress gameProgress, String fileName) {
+        try (FileOutputStream fos = new FileOutputStream(fileName);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(gameProgress);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 
+    public static void delete(String fileName) {
+        File newDir = new File(fileName);
+        newDir.delete();
+    }
+
+    public static void doZip(ArrayList<String> file, ArrayList<String> fileZip) {
+        try {
+            ZipOutputStream zout = new ZipOutputStream(new FileOutputStream("C:/Users/aleks/IdeaProjects/Setup/Games/savegames/zip_output.zip"));
+            for (int i = 0; i < file.size(); i++) {
+                try (FileInputStream fis = new FileInputStream(file.get(i))) {
+                    ZipEntry entry = new ZipEntry(fileZip.get(i));
+                    zout.putNextEntry(entry);
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
+                    zout.write(buffer);
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+            zout.close();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
     public static void main(String[] args) {
@@ -13,58 +44,24 @@ public class Main {
         GameProgress game2 = new GameProgress(29, 20, 26, 2.1);
         GameProgress game3 = new GameProgress(39, 34, 35, 3.3);
 
-        try (FileOutputStream fos1 = new FileOutputStream("C:/Users/aleks/IdeaProjects/Setup/Games/savegames/save1.dat");
-             ObjectOutputStream oos = new ObjectOutputStream(fos1)) {
-            oos.writeObject(game1);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+        ArrayList<String> file = new ArrayList<String>();
+        file.add("C:/Users/aleks/IdeaProjects/Setup/Games/savegames/save1.dat");
+        file.add("C:/Users/aleks/IdeaProjects/Setup/Games/savegames/save2.dat");
+        file.add("C:/Users/aleks/IdeaProjects/Setup/Games/savegames/save3.dat");
 
-        try (FileOutputStream fos2 = new FileOutputStream("C:/Users/aleks/IdeaProjects/Setup/Games/savegames/save2.dat");
-             ObjectOutputStream oos = new ObjectOutputStream(fos2)) {
-            oos.writeObject(game2);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+        ArrayList<String> fileZip = new ArrayList<String>();
+        fileZip.add("packed_save1.dat");
+        fileZip.add("packed_save2.dat");
+        fileZip.add("packed_save3.dat");
 
-        try (FileOutputStream fos3 = new FileOutputStream("C:/Users/aleks/IdeaProjects/Setup/Games/savegames/save3.dat");
-             ObjectOutputStream oos = new ObjectOutputStream(fos3)) {
-            oos.writeObject(game3);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+        for (String files : file) {
+            saveGame(game1, files);
         }
+        doZip(file, fileZip);
 
-        try (ZipOutputStream zout = new ZipOutputStream(new
-                FileOutputStream("C:/Users/aleks/IdeaProjects/Setup/Games/savegames/zip_output.zip"));
-             FileInputStream fis = new FileInputStream("C:/Users/aleks/IdeaProjects/Setup/Games/savegames/save1.dat");
-             FileInputStream fis2 = new FileInputStream("C:/Users/aleks/IdeaProjects/Setup/Games/savegames/save2.dat");
-             FileInputStream fis3 = new FileInputStream("C:/Users/aleks/IdeaProjects/Setup/Games/savegames/save3.dat")) {
-            ZipEntry entry = new ZipEntry("packed_save1.dat");
-            zout.putNextEntry(entry);
-            byte[] buffer = new byte[fis.available()];
-            byte[] buffer2 = new byte[fis2.available()];
-            byte[] buffer3 = new byte[fis3.available()];
-            fis.read(buffer);
-            zout.write(buffer);
-            ZipEntry entry2 = new ZipEntry("packed_save2.dat");
-            zout.putNextEntry(entry2);
-            fis2.read(buffer2);
-            zout.write(buffer2);
-            ZipEntry entry3 = new ZipEntry("packed_save3.dat");
-            zout.putNextEntry(entry3);
-            fis3.read(buffer3);
-            zout.write(buffer3);
-            zout.closeEntry();
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+        for (String files : file) {
+            delete(files);
         }
-        File newDir = new File("C:/Users/aleks/IdeaProjects/Setup/Games/savegames/save1.dat");
-        newDir.delete();
-        File newDir2 = new File("C:/Users/aleks/IdeaProjects/Setup/Games/savegames/save2.dat");
-        newDir2.delete();
-        File newDir3 = new File("C:/Users/aleks/IdeaProjects/Setup/Games/savegames/save3.dat");
-        newDir3.delete();
-
 
     }
 }
